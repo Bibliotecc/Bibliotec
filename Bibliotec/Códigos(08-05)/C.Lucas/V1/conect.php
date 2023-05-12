286ff7c9a6cce5403e82d4518b7c6557ff926013
Feh1411 <77704426+Feh1411@users.noreply.github.com>
2023-05-11 21:15:59 -0300

    Add files via upload

---
+++ Bibliotec/Códigos(08-05)/C.Lucas/V1/conect.php
@@ -0,0 +1,10 @@
+<?php
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '1234';
+    $dbName = 'teste';
+    $porta = '3307';
+    
+    $conn = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName,$porta);
+
+?>
