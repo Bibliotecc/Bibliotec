3939a3b3a980c8bfd32b063ae7567c8b1f31b7dd
etim <etim@etecolimpia.com>
2023-11-16 08:40:40 -0300

    AAAAAAAA

--- SistemBiblio/hometcc/admin/js/historicoEmp.js
+++ SistemBiblio/hometcc/admin/js/historicoEmp.js
@@ -24,9 +24,7 @@ var ultimaBarra = urlArray[urlArray.length - 1];
 
 const corpo = document.getElementById('corpo');
 function Emprestimos(nomeLivro, usuNome, rm, dataPedido, idEmprestimo, statusEmp) {
-    if (psq === rm || psq === nomeLivro || psq === usuNome){
-
-        
+    if (psq === rm || psq === nomeLivro || psq === usuNome){        
         const tr = document.createElement("tr");
         const tdRm = document.createElement("td");
             tdRm.innerText = rm;
